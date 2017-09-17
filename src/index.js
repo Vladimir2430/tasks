27b921428e57a5b8fa066a688306d3b7b9a044ef
Vladimir <31570004+Vladimir2430@users.noreply.github.com>
2017-09-17 23:56:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function multiply(first, second) {
-  alert(string(number(first)*number(second)));
+let arrBig = second.split('').map(s => {	
+	return first.split('').map(f => {
+		return (+s)*(+f);
+	}).reverse(); 
+});
+
+arrBig.reverse();
+
+let ddd=[];
+for (let j=0; j<arrBig.length; j++){
+	if (j===0) {
+		ddd = arrBig[0];
+	} else {
+		ddd.push(0);
+		for(let k=0; k<arrBig[j].length;k++){
+			ddd[j+k] = ddd[j+k] + arrBig[j][k];
+		}
+	}
+};
+
+let strng = '';
+for (let i=0; i<ddd.length; i++) {
+	if (ddd[i] < 10 ) {
+		strng += ddd[i];
+	} else if (i === ddd.length-1) {
+		let lastPart = (''+ddd[i]).split('').reverse().join('');
+		strng += lastPart;
+	} else {
+		let partString = ddd[i]%10;
+		ddd[i+1] += (ddd[i]- partString)/10;
+		strng += partString;
+	}
+};
+return strng.split('').reverse().join('');
 }
