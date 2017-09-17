9026870c097966c6da8a6333ee33c6fd5f415dff
Vladimir <31570004+Vladimir2430@users.noreply.github.com>
2017-09-18 00:27:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 module.exports = function multiply(first, second) {
-let arrBig = second.split('').map(s => {	
+let arr = second.split('').map(s => {	
 	return first.split('').map(f => {
 		return (+s)*(+f);
 	}).reverse(); 
 });
 
-arrBig.reverse();
+arr.reverse();
 
-let ddd=[];
-for (let j=0; j<arrBig.length; j++){
+let a=[];
+for (let j=0; j<arr.length; j++){
 	if (j===0) {
-		ddd = arrBig[0];
+		a = arr[0];
 	} else {
-		ddd.push(0);
-		for(let k=0; k<arrBig[j].length;k++){
-			ddd[j+k] = ddd[j+k] + arrBig[j][k];
+		a.push(0);
+		for(let k=0; k<arr[j].length;k++){
+			a[j+k] = a[j+k] + arr[j][k];
 		}
 	}
 };
 
 let strng = '';
-for (let i=0; i<ddd.length; i++) {
-	if (ddd[i] < 10 ) {
-		strng += ddd[i];
-	} else if (i === ddd.length-1) {
-		let lastPart = (''+ddd[i]).split('').reverse().join('');
+for (let i=0; i<a.length; i++) {
+	if (a[i] < 10 ) {
+		strng += a[i];
+	} else if (i === a.length-1) {
+		let lastPart = (''+a[i]).split('').reverse().join('');
 		strng += lastPart;
 	} else {
-		let partString = ddd[i]%10;
-		ddd[i+1] += (ddd[i]- partString)/10;
+		let partString = a[i]%10;
+		a[i+1] += (a[i]- partString)/10;
 		strng += partString;
 	}
 };
