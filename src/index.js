59d8a4ab40b1bffafe23c50f3bf50227c6828423
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-23 01:59:54 +0300

    updated code

--- src/index.js
+++ src/index.js
@@ -1,33 +1,26 @@
 module.exports = function multiply(first, second) {
-let a = second.split('').map(s => {	
-	return first.split('').map(f => {
-		return (+s)*(+f);
-	}).reverse(); 
-});
-a.reverse();
-let b =[];
-for (let j=0; j<a.length; j++){
-	if (j===0) {
-		b = a[0];
-	} else {
-		b.push(0);
-		for(let k=0; k<a[j].length;k++){
-			b[j+k] += a[j][k];
-		}
-	}
-};
-let strng = '';
-for (let i=0; i<b.length; i++) {
-	if (b[i] < 10 ) {
-		strng += b[i];
-	} else if (i === b.length-1) {
-		let lastPart = (''+b[i]).split('').reverse().join('');
-		strng += lastPart;
-	} else {
-		let partString = b[i]%10;
-		b[i+1] += (b[i]- partString)/10;
-		strng += partString;
-	}
-};
-return strng.split('').reverse().join('');
+  let a = second.split('').map((s) => first.split('').map((f) => (+s) * (+f)).reverse()).reverse();
+
+  let b =[];
+  for (let j = 0; j < a.length; j++) {
+	  if (j === 0) {b = a[0];
+	  } else {
+		  b.push(0);
+		  for (let k = 0; k < a[j].length; k++) {
+			  b[j+k] += a[j][k];
+		  }
+	  }
+  };
+
+  let c = '';
+  for (let i = 0; i < b.length; i++) {
+	  if (b[i] < 10) {c += b[i];
+    } else if (i === b.length-1) {c += b[i].toString().split('').reverse().join('');
+	  } else {
+      c += b[i] % 10;
+		  b[i+1] += (b[i] - b[i] % 10) / 10;
+	  }
+  };
+
+  return c.split('').reverse().join('');
 }
